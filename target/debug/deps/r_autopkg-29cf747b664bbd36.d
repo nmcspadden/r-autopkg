67c3eec408f1ccce5a7b3266ca977f3a5cd7f6ae
Nick McSpadden <nmcspadden@gmail.com>
2024-07-22 11:11:21 -0400

    Added initial cargo files

---
+++ target/debug/deps/r_autopkg-29cf747b664bbd36.d
@@ -0,0 +1,5 @@
+/Users/nmcspadden/Documents/GitHub/r-autopkg/target/debug/deps/libr_autopkg-29cf747b664bbd36.rmeta: src/main.rs
+
+/Users/nmcspadden/Documents/GitHub/r-autopkg/target/debug/deps/r_autopkg-29cf747b664bbd36.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/r_autopkg-c0fe20dafe4dddb3.d
@@ -0,0 +1,5 @@
+/Users/nmcspadden/Documents/GitHub/r-autopkg/target/debug/deps/libr_autopkg-c0fe20dafe4dddb3.rmeta: src/main.rs
+
+/Users/nmcspadden/Documents/GitHub/r-autopkg/target/debug/deps/r_autopkg-c0fe20dafe4dddb3.d: src/main.rs
+
+src/main.rs:
